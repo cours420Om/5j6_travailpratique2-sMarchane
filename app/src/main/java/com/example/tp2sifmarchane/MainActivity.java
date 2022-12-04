d45559364d5ea3a47c12ea1eb0b1d915371ca6f3
sif marchane <89397625+sMarchane@users.noreply.github.com>
2022-12-03 22:02:23 -0500

    Test

---
+++ app/src/main/java/com/example/tp2sifmarchane/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.tp2sifmarchane;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
